b641f445ebfb8113e8205436a63f25d8e6269954
jonathan-julio <jonathan.julio@ufrn.edu.br>
2019-11-21 18:09:58 -0300

    so falta função remover e outros animais

---
+++ build/tratador.h
@@ -0,0 +1,29 @@
+#define _GLIBCXX_USE_CXX11_ABI 0
+#ifndef TRATADOR_H
+#define TRATADOR_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <iterator>
+#include <algorithm>
+
+
+
+using namespace std;
+
+class Tratador:public Funcionario{
+	protected:
+		int nivel_seguranca;
+
+//Getters e Setters
+	public: 	
+
+		int getNivel_seguranca();		 	
+		void setNivel_seguranca();		 
+
+};
+
+
+#endif // TRATADOR_H
--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,18 @@ using namespace std;
 gerenciar feira;
 
 int main (int argc, char const *argv[]){
-
+	//g++ -o exe main.cpp anfibioNativo.cpp animal.cpp funcionario.cpp gerenciar.cpp veterinario.cpp anfibio.cpp
 	int opcao = -1;
 	int opcao2 = 0;
 	while (opcao!= 0){
 		cout << endl << "++++++++++++++++++++++++++++++++"<<endl
 		<< endl << "Escolha a opcao desejada"<<endl	
 		<< "Digite 1 - Cadastrar um novo animal a ser comercializado "<< endl
-		<< "Digite 2 - Remover animal já comercializado "<<endl
+		<< "Digite 2 - Remover animal ja comercializado "<<endl
 		<< "Digite 3 - Alterar dados cadastrais de um animal "<<endl
 		<< "Digite 4 - Consultar os dados cadastrais de um determinado animal ou por uma classe de animal "<< endl
-		<< "Digite 5 - Consultar animais sob responsabilidade de um determinado veterinário ou tratador "<< endl
-		<< "Digite 6 - cadastrar veterinário ou tratador "<< endl
+		<< "Digite 5 - Consultar animais sob responsabilidade de um determinado veterinario ou tratador "<< endl
+		<< "Digite 6 - cadastrar veterinario ou tratador "<< endl
 		<< "Digite 0 - Sair"<< endl
 		<< "++++++++++++++++++++++++++++++++"<<endl
 		<< endl<< "Digite sua Escolha: " <<endl;
@@ -39,13 +39,13 @@ int main (int argc, char const *argv[]){
 					
 				break;	
 			case 3: 
-					
+				feira.alterarDadosAnimal();
 				break;	
 			case 4:
-					
+					feira.consutarDados();
 				break;
 			case 5:
-					
+				feira.buscaAnimalFuncionario();
 				break;
 			case 6:
 				feira.addFuncionario();
